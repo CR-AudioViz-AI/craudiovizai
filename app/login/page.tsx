4b0125a552472d998c84702a2f3168b96e94dd7b
Old <225056366+roy-henderson@users.noreply.github.com>
2025-12-25 20:28:01 -0500

    feat: Add Twitch OAuth + Facebook placeholder - Dec 25 2025 12:00 PM EST

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -98,6 +98,32 @@ const OAUTH_PROVIDERS = [
     textColor: 'text-white',
     borderColor: 'border-[#5865F2]',
   },
+  {
+    id: 'twitch',
+    name: 'Twitch',
+    enabled: true,
+    icon: (
+      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+        <path d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z"/>
+      </svg>
+    ),
+    bgColor: 'bg-[#9146FF] hover:bg-[#7C2BFF]',
+    textColor: 'text-white',
+    borderColor: 'border-[#9146FF]',
+  },
+  {
+    id: 'facebook',
+    name: 'Facebook',
+    enabled: false, // Will be enabled when configured
+    icon: (
+      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+        <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+      </svg>
+    ),
+    bgColor: 'bg-[#1877F2] hover:bg-[#166FE5]',
+    textColor: 'text-white',
+    borderColor: 'border-[#1877F2]',
+  },
 ];
 
 function LoginForm() {
